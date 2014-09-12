c7adf0a2988290e21220bd31a34e5d7a04d005a0
Mark Feltner <mark@feltner.me>
2014-09-12 02:19:09 -0500

    test($Bot): Add basic bot tests (shh! they're blank!)

---
+++ test/bot-test.js
@@ -0,0 +1,40 @@
+var
+    test = require('tape'),
+    _ = require('lodash')
+    ;
+
+var
+    Bot = require('../lib/bot')
+    ;
+
+test.skip('$Bot', function(t){
+
+    t.test('#constructor', function(st){
+
+    });
+
+    t.test('#listen()', function(st){
+
+    });
+
+    t.test('#onPing()', function(st){
+
+    });
+
+    t.test('#loadPlugins()', function(st){
+
+    });
+
+    t.test('#onIncomingMessage()', function(st){
+
+    });
+
+    t.test('#handleMessage()', function(st){
+
+    });
+
+    t.test('#sendMessage()', function(st){
+
+    });
+
+});
