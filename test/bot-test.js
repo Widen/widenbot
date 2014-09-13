aa5631ede528f47605415a84d9e521142d09352d
Mark Feltner <mark@feltner.me>
2014-09-12 20:27:57 -0500

    test($Bot): Test bot functionality

--- test/bot-test.js
+++ test/bot-test.js
@@ -1,40 +1,166 @@
 var
     test = require('tape'),
+    http = require('http'),
+    request = require('request'),
     _ = require('lodash')
     ;
+
+var PORT = 8000,
+    URL = 'http://127.0.0.1:'+PORT,
+    HOOK_URL = 'http://127.0.0.1:'+(PORT+1),
+    TRIGGER = '!',
+    MESSAGE_URL = URL + '/message'
+    ;
+
+var MockOptions = function(opts)
+{
+    var defaults = {
+        botname: 'mockbot',
+        port: PORT,
+        token: 'fooToken',
+        url: HOOK_URL,
+        logging: { console: false, path: './log/test.log'}
+    };
+
+    return _.extend(defaults, opts);
+};
+
+var MockIncomingMessageData = function(opts)
+{
+    var defaults = {
+        token: '400T0K3N',
+        team_id: '400T3AM',
+        team_domain: 'footeam',
+        channel_id: '400CH4NN3L',
+        channel_name: 'foochannel',
+        timestamp: new Date(),
+        user_id: '400US34',
+        user_name: 'foouser',
+        text: '',
+        trigger_word: TRIGGER
+    };
+
+    return _.extend(defaults, opts);
+
+};
+
+var MockOutgoingMessageData = function(opts)
+{
+    var defaults = {
+        channel: 'foochannel',
+        username: 'testbot',
+        text: ''
+    };
+
+    return _.extend(defaults, opts);
+};
 
 var
     Bot = require('../lib/bot')
     ;
 
-test.skip('$Bot', function(t){
+test('$Bot', function(t){
 
     t.test('#constructor', function(st){
+        st.plan(2);
+
+        st.throws(function(){
+            new Bot();
+        });
+
+        st.doesNotThrow(function(){
+            var bot, options;
+
+            options = MockOptions();
+            bot = new Bot(options);
+        });
 
     });
 
     t.test('#listen()', function(st){
+        st.plan(3);
+
+        st.doesNotThrow(function(){
+            var bot, options;
+
+            options = MockOptions();
+            bot = new Bot(options);
+            bot.listen(function(){
+                st.ok(true, "Bot listens without error");
+            });
+            bot.close(function(){
+                st.ok(true, "Bot closes without error");
+            });
+        });
 
     });
 
     t.test('#onPing()', function(st){
+        st.plan(2);
+        var bot, options;
+
+        options = MockOptions();
+        bot = new Bot(options);
+
+        bot.listen(function(){
+            request({
+                method: 'GET',
+                uri: URL + '/ping',
+            }, function(err, res, body){
+                console.log(URL+'/ping');
+                st.equal(res.statusCode, 200);
+                st.equal(body, '"pong"');
+                bot.close();
+            });
+        });
 
     });
 
-    t.test('#loadPlugins()', function(st){
+    t.test('#onIncomingMessage(),#handleMessage(),#sendMessage()', function(st){
+        st.plan(2);
+        var bot, options, body;
 
-    });
+        options = MockOptions({ plugins: { 'echo': {} }});
+        bot = new Bot(options);
+        body = MockIncomingMessageData({ text: '!echo foo' });
 
-    t.test('#onIncomingMessage()', function(st){
+        bot.listen(function(){
 
-    });
+            var server = http.createServer();
 
-    t.test('#handleMessage()', function(st){
+            var concat = require('concat-stream');
 
-    });
+            server.on('connection', function(socket){
+                socket.setTimeout(400);
+            });
 
-    t.test('#sendMessage()', function(st){
+            server.on('request', function(req, res){
+                bot.close(function(){ console.log('bot closed'); });
+                server.close(function(){ console.log('server closed'); });
+                var write = concat(function(data){
+                    st.deepEqual({
+                        'link_names': 1,
+                        'unfurl_links': true,
+                        'channel': '#' + body.channel_name,
+                        'username': options.botname,
+                        'text': 'foo'
+                    }, JSON.parse(data.toString()));
+                });
+                req.pipe(write);
+                res.statusCode = 200;
+                res.end();
+            });
 
+            server.listen(PORT+1, function(){
+                request({
+                    method: 'POST',
+                    uri: MESSAGE_URL,
+                    json: body
+                }, function(err, res, body){
+                    st.equal(res.statusCode, 200, "Should return a 200 status to messages it can respond to");
+                });
+            });
+        });
     });
 
 });
