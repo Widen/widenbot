00c937876257a284e60753c0b94a0ea55a4c0515
Mark Feltner <mark@feltner.me>
2014-09-14 16:57:06 -0500

    feat($plugins/last.fm): Add brain integration
    Users can register to link their chat names with their Last.FM names.
    Users can call nowplaying and the plugin will remember their lastfm username.

--- plugins/last.fm.js
+++ plugins/last.fm.js
@@ -49,47 +49,72 @@ var LastFm = module.exports = {
             if (subCommand === 'register' && subArgs && subArgs[0])
             {
                 lastFmUsername = subArgs[0];
+                var slackUsername = ctx.incoming_message.user_name;
                 // register lastFmUsername in database
+                if (lastFmUsername)
+                {
+                    return new promise(function(resolve, reject){
+                        ctx.brain.put(slackUsername, lastFmUsername, function(){
+                            resolve("username " + lastFmUsername + " register to " + slackUsername);
+                        });
+                    });
+                }
                 return 'in progress';
             }
             else if (subCommand === 'nowplaying' || subCommand === 'np')
             {
-                var username = ctx.incoming_message.user_name;
-                if (subArgs && subArgs[0])
-                {
-                    lastFmUsername = subArgs[0];
-                }
 
                 return new promise(function(resolve, reject)
                 {
-                    lastFm.request('user.getRecentTracks', {
-                        'user': lastFmUsername,
-                        'limit': 1,
-                        handlers: {
-                            success: function(data)
-                            {
-                                var track = data.recenttracks.track;
 
-                                // could not return an array despite the limit
-                                if (track.length) {
-                                    track = track[0];
-                                }
-
-                                var
-                                    album = track.album['#text'],
-                                    name = track.name,
-                                    artist = track.artist['#text'],
-                                    url = track.url,
-                                    imgUrl = track.image[2]['#text']
-                                    ;
-
-                                var post = '[ *' + name + '* ] _by_ [ *' + artist + '* ] _on_ [ *' + album + '* ] - ' + imgUrl;
-                                resolve(post);
-
-
-                            },
-                            error: reject
+                    var username = ctx.incoming_message.user_name;
+                    //if (subArgs && subArgs[0])
+                    //{
+                    //    lastFmUsername = subArgs[0];
+                    //}
+                    //else
+                    //{
+                    //}
+                    ctx.brain.get(username, function(err, val){
+                        if (err) {
+                            ctx.log.error(err);
+                            resolve("username " + username + " has not registered for lastfm");
                         }
+
+                        lastFmUsername = val;
+
+                        lastFm.request('user.getRecentTracks', {
+                            'user': lastFmUsername,
+                            'limit': 1,
+                            handlers: {
+                                success: function(data)
+                                {
+                                    var track = data.recenttracks.track;
+
+                                    // could not return an array despite the limit
+                                    if (track.length) {
+                                        track = track[0];
+                                    }
+
+                                    var
+                                        album = track.album['#text'],
+                                        name = track.name,
+                                        artist = track.artist['#text'],
+                                        url = track.url,
+                                        imgUrl = track.image[2]['#text']
+                                        ;
+
+                                    var post = '[ *' + name + '* ] _by_ [ *' + artist + '* ] _on_ [ *' + album + '* ] - ' + imgUrl;
+                                    ctx.log.info(post);
+                                    resolve(post);
+
+                                },
+                                error: function(e) {
+                                    ctx.log.error(e);
+                                    reject(e);
+                                }
+                            }
+                        });
                     });
                 });
             }
