ab7b29991f432a85e7482f562e88e76babca74ae
Mark Feltner <mark@feltner.me>
2014-09-16 01:31:21 -0500

    docs(config.example.js): add webhooks key

--- config.example.js
+++ config.example.js
@@ -35,7 +35,9 @@ module.exports = {
             'api_key': process.env.LASTFM_API_KEY,
             'secret': process.env.LASTFM_SECRET
         }
+    },
 
-        //'foo': {}
+    webhooks: {
+        'echo': {}
     }
 };
