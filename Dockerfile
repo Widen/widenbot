ad413355c805f7c39e9690f7b38fbd97157ae700
Mark Feltner <mfeltner@widen.com>
2014-11-25 11:11:40 -0600

    chore: Make deployable on AWS Elasticbeanstalk

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,15 @@
 #
 # https://github.com/Widen/widenbot
 #
+FROM node
 
-FROM dockerfile/nodejs
+#RUN groupadd -r widenbot && useradd -r -g widenbot widenbot
+
+COPY . /opt/widenbot
+WORKDIR /opt/widenbot
+
+EXPOSE 8000
+
+RUN ["npm", "install"]
+
+CMD ["npm", "start"]
--- config.js
+++ config.js
@@ -1,32 +1,24 @@
 module.exports = {
-    name: 'widenbot',
+    botname: 'widenbot',
     icon: ':ghost:',
 
     env: 'dev',
-    repl: false,
-
     port: 8000,
-
     token: process.env.SLACK_TOKEN,
     url: process.env.SLACK_URL,
-
     brain: {
         dbpath: './db'
     },
-
     logging: {
         console: true,
-        path: './log'
+        path: './log',
+        logentries: {
+            token: process.env.LOGENTRIES_TOKEN
+        }
     },
-
     plugins: {
+        'help': {},
         'echo': {},
-        'gif': {
-            GIPHY_API_KEY: process.env.GIPHY_API_KEY
-        },
-        'coolface': {},
-        'flipit': {},
-        'coolface': {},
         'nowplaying': {
             'api_key': process.env.LASTFM_API_KEY,
             'secret': process.env.LASTFM_SECRET
@@ -34,10 +26,11 @@ module.exports = {
         'last.fm': {
             'api_key': process.env.LASTFM_API_KEY,
             'secret': process.env.LASTFM_SECRET
-        }
-    },
-
-    webhooks: {
-        'echo': {}
+        },
+        'gif': {
+            api_key: process.env.GIPHY_API_KEY
+        },
+        'flipit': {},
+        'coolface': {},
     }
 };
