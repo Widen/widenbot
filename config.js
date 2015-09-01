74bd25c5a3f96e4dabcfb0eb7a4bc70df5da9113
Mark Feltner <mfeltner@widen.com>
2015-08-31 21:48:36 -0500

    feat(plugins): modularize plugins

--- config.js
+++ config.js
@@ -1,20 +1,6 @@
 module.exports = {
     plugins: {
         'help': {},
-        'echo': {},
-        'ermahgerd': {},
-        'nowplaying': {
-            'api_key': process.env.LASTFM_API_KEY,
-            'secret': process.env.LASTFM_SECRET
-        },
-        'last.fm': {
-            'api_key': process.env.LASTFM_API_KEY,
-            'secret': process.env.LASTFM_SECRET
-        },
-        'gif': {
-            api_key: process.env.GIPHY_API_KEY
-        },
-        'flipit': {},
-        'coolface': {},
+        'echo': {}
     }
 };
